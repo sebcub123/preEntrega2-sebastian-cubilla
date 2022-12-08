8d826d2edd77bd624a894d2b6d89a212638692f0
Sebastian Cubilla <sebcub123@gmail.com>
2022-12-08 20:23:43 -0300

    agregando funciones

--- js/app.js
+++ js/app.js
@@ -1,95 +1,217 @@
-let nombreUsuario = prompt("¿Cuál es tu nombre?").toLowerCase();
-
-alert("Hola " + nombreUsuario + " Bienvenido");
-alert(`${nombreUsuario}, en el siguiente listado  vas a ver los productos que tenemos disponibles. 
-Por favor ingresa el numero del producto que queres adquirir`);
-  
-// creamos catalogo de productos
-let catalogo = [
-  {
-    nombre: "Protector Yilho",
-    precio: 1156,
-  },
-  {
-    nombre: "Acondicionador keratina",
-    precio: 595,
-  },
-  {
-    nombre: "Guantes nitrilo",
-    precio: 50,
-  },
-  {
-    nombre: "Crema Mav",
-    precio: 986,
-  },
-  {
-    nombre: "Ionizado nov",
-    precio: 663,
-  },
-  {
-    nombre: "Crema multi",
-    precio: 765,
-  },
-  {
-    nombre: "Crema peinar argan",
-    precio: 833,
-  },
-  {
-    nombre: "serum bekim",
-    precio: 1411,
-  },
-  {
-    nombre: "Crema peinar rbx",
-    precio: 544,
-  }, 
-];
-
-
-// carrito vacio
 let carrito = [];
 
+const contenedor = document.querySelector("#contenedor");
+const carritoContenedor = document.querySelector("#carritoContenedor");
+const vaciarCarrito = document.querySelector("#vaciarCarrito");
+const precioTotal = document.querySelector("#precioTotal");
+const activarFuncion = document.querySelector("#activarFuncion");
+const procesarCompra = document.querySelector("#procesarCompra");
+const totalProceso = document.querySelector("#totalProceso");
+const formulario = document.querySelector('#procesar-pago')
 
-// recorrido y vista de catalogo
-const verCatalogo = () => {
-  let productos = "";
-  for (let i = 0; i < catalogo.length; i++) {
-    productos += `${i} - ${catalogo[i].nombre} - $${catalogo[i].precio} \n`;
-  }
-  return productos;
-};
-let productoElegido = prompt(verCatalogo());
+if (activarFuncion) {
+  activarFuncion.addEventListener("click", procesarPedido);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+  mostrarCarrito();
+  document.querySelector("#activarFuncion").click(procesarPedido);
+});
+if (formulario) {
+  formulario.addEventListener('submit', enviarCompra)
+}
 
 
-//añadir productos al carrito 
-carrito.push(catalogo[productoElegido]);
-console.log(carrito);
+if (vaciarCarrito) {
+  vaciarCarrito.addEventListener("click", () => {
+    carrito.length = [];
+    mostrarCarrito();
+  });
+}
 
-alert(`${nombreUsuario}, agregaste ${catalogo[productoElegido].nombre} por un valor de: $${catalogo[productoElegido].precio}`);
-
-let compraOtroProducto = prompt("¿Queres comprar otro producto? Escribi si o no");
-
-
-//volver a mostrar el catalogo para elegir otro producto, si no, se muestran los productos y valor total 
-const comprarOtroProducto = () => {
-  if (compraOtroProducto == "si") {
-    productoElegido = prompt(verCatalogo());
-    carrito.push(catalogo[productoElegido]);
-    console.log(carrito);
-    alert(`${nombreUsuario}, agregaste ${catalogo[productoElegido].nombre} por un valor de: $${catalogo[productoElegido].precio}`);
-    compraOtroProducto = prompt("¿Deseas comprar otro producto? Responda si o no");
-    comprarOtroProducto();
-  } else { 
-    const mostrarCarrito = () => { 
-      let subTotal = `${nombreUsuario}, compraste satisfactoriamente: \n`;
-      carrito.forEach((producto) => {
-        subTotal += `${producto.nombre} - $${producto.precio} \n`;
+if (procesarCompra) {
+  procesarCompra.addEventListener("click", () => {
+    if (carrito.length === 0) {
+      Swal.fire({
+        title: "¡Tu carrito está vacio!",
+        text: "Compra algo para continuar con la compra",
+        icon: "error",
+        confirmButtonText: "Aceptar",
       });
-      const total = carrito.reduce((acc, item) => acc + item.precio, 0);
-      ""
-      subTotal += `EL TOTAL A PAGAR ES: $${total}`;
-      return subTotal;
-    };
-    alert(mostrarCarrito());
+    } else {
+      location.href = "compra.html";
+    }
+  });
+}
+
+stockProductos.forEach((prod) => {
+  const { id, nombre, precio, desc, img, cantidad } = prod;
+  if (contenedor) {
+    contenedor.innerHTML += `
+    <div class="card mt-3" style="width: 18rem;">
+    <img class="card-img-top mt-2" src="${img}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">${nombre}</h5>
+      <p class="card-text">Precio: ${precio}</p>
+      <p class="card-text">Descripcion: ${desc}</p>
+      <p class="card-text">Cantidad: ${cantidad}</p>
+      <button class="btn btn-primary" onclick="agregarProducto(${id})">Comprar Producto</button>
+    </div>
+  </div>
+    `;
   }
+});
+
+const agregarProducto = (id) => {
+  const existe = carrito.some(prod => prod.id === id)
+
+  if (existe) {
+    const prod = carrito.map(prod => {
+      if (prod.id === id) {
+        prod.cantidad++
+      }
+    })
+  } else {
+    const item = stockProductos.find((prod) => prod.id === id)
+    carrito.push(item)
+  }
+  mostrarCarrito()
+
 };
-comprarOtroProducto();
+
+const mostrarCarrito = () => {
+  const modalBody = document.querySelector(".modal .modal-body");
+  if (modalBody) {
+    modalBody.innerHTML = "";
+    carrito.forEach((prod) => {
+      const { id, nombre, precio, desc, img, cantidad } = prod;
+      console.log(modalBody);
+      modalBody.innerHTML += `
+      <div class="modal-contenedor">
+        <div>
+          <img class="img-fluid img-carrito" src="${img}"/>
+        </div>
+        <div>
+          <p>Producto: ${nombre}</p>
+          <p>Precio: ${precio}</p>
+          <p>Cantidad :${cantidad}</p>
+          <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Eliminar producto</button>
+        </div>
+      </div>
+      `;
+    });
+  }
+
+  if (carrito.length === 0) {
+    /*console.log("Nada");*/
+    modalBody.innerHTML = `
+    <p class="text-center text-primary parrafo">¡Aun no agregaste nada!</p>
+    `;
+  } else {
+    console.log("Algo");
+  }
+  carritoContenedor.textContent = carrito.length;
+
+  if (precioTotal) {
+    precioTotal.innerText = carrito.reduce(
+      (acc, prod) => acc + prod.cantidad * prod.precio,
+      0
+    );
+  }
+
+  guardarStorage();
+};
+
+function guardarStorage() {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+function eliminarProducto(id) {
+  const productoId = id;
+  carrito = carrito.filter((prod) => prod.id !== productoId);
+  mostrarCarrito();
+}
+function procesarPedido() {
+  carrito.forEach((prod) => {
+    const listaCompra = document.querySelector("#lista-compra tbody");
+    const { id, nombre, precio, img, cantidad } = prod;
+    if (listaCompra) {
+      const row = document.createElement("tr");
+      row.innerHTML += `
+              <td>
+                <img class="img-fluid img-carrito" src="${img}"/>
+              </td>
+              <td>${nombre}</td>
+              <td>${precio}</td>
+              <td>${cantidad}</td>
+              <td>${precio * cantidad}</td>
+            `;
+      listaCompra.appendChild(row);
+    }
+  });
+  totalProceso.innerText = carrito.reduce(
+    (acc, prod) => acc + prod.cantidad * prod.precio,
+    0
+  );
+}
+
+function enviarCompra(e) {
+  e.preventDefault()
+  const cliente = document.querySelector('#cliente').value
+  const email = document.querySelector('#correo').value
+
+  if (email === '' || cliente == '') {
+    Swal.fire({
+      title: "¡Debes completar tu email y nombre!",
+      text: "Rellena el formulario",
+      icon: "error",
+      confirmButtonText: "Aceptar",
+    })
+  } else {
+
+    const btn = document.getElementById('button');
+
+    // document.getElementById('procesar-pago')
+      //.addEventListener('submit', function(event) {
+      // event.preventDefault();
+
+    btn.value = 'Enviando...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_qxwi0jn';
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Finalizar compra';
+        alert('Correo enviado!');
+      }, (err) => {
+        btn.value = 'Finalizar compra';
+        alert(JSON.stringify(err));
+      });
+
+    const spinner = document.querySelector('#spinner')
+    spinner.classList.add('d-flex')
+    spinner.classList.remove('d-none')
+
+    setTimeout(() => {
+      spinner.classList.remove('d-flex')
+      spinner.classList.add('d-none')
+      formulario.reset()
+
+      const alertExito = document.createElement('p')
+      alertExito.classList.add('alert', 'alerta', 'd-block', 'text-center', 'col-12', 'mt-2', 'alert-success')
+      alertExito.textContent = 'Compra realizada correctamente'
+      formulario.appendChild(alertExito)
+
+      setTimeout(() => {
+        alertExito.remove()
+      }, 5000)
+
+
+   },  5000)
+  }
+  localStorage.clear()
+
+}
